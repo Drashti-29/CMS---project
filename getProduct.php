38cd9906565257038b27726698da820c7a8d62d2
Marcus Jeong <marcus.jeong96@gmail.com>
2024-11-21 14:58:39 -0500

    added images for above the fold sections

--- getProduct.php
+++ getProduct.php
@@ -29,7 +29,30 @@
 
     $query = "SELECT * FROM products WHERE productLine = '$productLine'";
     $products = mysqli_query($connect, $query);
-
+    echo '<div class="row mb-3">';
+      if ($productLine == "Classic Cars") {
+        echo '<img src="https://images.unsplash.com/photo-1515281239448-2abe329fe5e5?q=80&w=1193&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">';
+      } elseif ($productLine == "Motorcycles") {
+        echo '<img src="https://images.unsplash.com/photo-1479909013849-e16a7dd14323?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">';
+      } elseif ($productLine == "Planes") {
+        echo '<img src="https://images.unsplash.com/photo-1702243080294-27e09e892995?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">';
+      } elseif ($productLine == "Ships") {
+        echo '<img src="https://images.unsplash.com/photo-1466188635785-8b5f35009981?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">';
+      } elseif ($productLine == "Trains") {
+        echo '<img src="https://images.unsplash.com/photo-1631819441984-f57278471d7f?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">';
+      } elseif ($productLine == "Trucks and Buses") {
+        echo '<img src="https://images.unsplash.com/photo-1532151581482-2ff889458a5e?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">';
+      } elseif ($productLine == "Vintage Cars") {
+        echo '<img src="https://images.unsplash.com/photo-1534325206648-bb8fa5dba108?q=80&w=1174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">';
+      };
+    echo '</div>';
+      // https://unsplash.com/photos/die-cast-car-collection-on-rack-JBrfoV-BZts <- Cars
+      // https://unsplash.com/photos/close-up-photo-of-harley-davidson-touring-motorcycle-MfkYKk07lnE <- Motorcycles
+      // https://unsplash.com/photos/a-garage-filled-with-lots-of-clutter-and-tools-Cw_MTo3o4hs <- Planes
+      // https://unsplash.com/photos/black-brown-and-white-galleon-ship-scale-model-on-brown-wooden-shelf-05nD5VT-714 <- Ships
+      // https://unsplash.com/photos/a-toy-train-is-sitting-on-the-tracks-uZ03bUfmhGU <- trains
+      // https://unsplash.com/photos/gray-truck-toy-with-brown-wooden-frame-on-wall-jW43ulaCnok <- trucks
+      // https://unsplash.com/photos/selective-focus-photography-of-classic-brown-coupe-die-cast-model-on-gray-ground-Ug2XO67KVYQ <- vintage car
     echo '<div class= "row">';
     foreach($products as $product){
       echo 
--- index.php
+++ index.php
@@ -33,6 +33,7 @@
     <!-- above fold picture -->
     <div class="row mb-3">
       <img src="https://images.unsplash.com/photo-1515281239448-2abe329fe5e5?q=80&w=1193&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="wall of model cars" >
+      <!-- https://unsplash.com/photos/die-cast-car-collection-on-rack-JBrfoV-BZts -->
     </div>
     <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4" id="cats">
       <?php 
